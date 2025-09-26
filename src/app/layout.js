0898e1227e4c8e0ac5bf1a243c38d9f7e42102f4
Junbeom Park <ephesi4ns623@gmail.com>
2025-09-26 20:21:11 +0900

    feat: add Vercel SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
